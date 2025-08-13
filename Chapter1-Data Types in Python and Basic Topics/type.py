7e93dce7be183a78f708b15be1528d6416b7c69c
Mahdi gosyan <mahdigosyanapple@gmail.com>
2025-08-13 15:47:22 +0330

    Create type.py

---
+++ Chapter1-Data Types in Python and Basic Topics/type.py
@@ -0,0 +1,18 @@
+my_int = 12
+my_float = 23.1
+my_string = "hi"
+my_list = [1,2,"hi",98]
+my_dict = {"key1" : 1 , "key2" : 33}
+my_boolean = True
+my_tuple = (1,2,4,3,2)
+my_set = {2,3,4,5}
+
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_string))
+print(type(my_list))
+print(type(my_dict))
+print(type(my_boolean))
+print(type(my_tuple))
+print(type(my_set))
