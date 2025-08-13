3808316b13f786aee9994abbeaa506144f974271
Mahdi gosyan <mahdigosyanapple@gmail.com>
2025-08-13 14:38:09 +0330

    dictionary in python

---
+++ Chapter1-Data Types in Python and Basic Topics/dictionary.py
@@ -0,0 +1,18 @@
+my_dict = {
+    "key1" : 1 ,
+    "key2" : "two",
+    "key3" : [1,4,9,2,3]
+}
+
+my_stu = {
+    "name" : "mahdi",
+    "age" : 22,
+    "number" : 912989000
+}
+
+print(my_dict["key1"])
+print(my_dict["key3"][2])
+
+print(my_stu.keys())
+
+print(my_stu.values())
