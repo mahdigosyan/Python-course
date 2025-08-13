7af5ed8f1349441f4ba9017f9bf39e706e62dddc
Mahdi gosyan <mahdigosyanapple@gmail.com>
2025-08-13 14:08:20 +0330

    list in python

---
+++ Chapter1-Data Types in Python and Basic Topics/list.py
@@ -0,0 +1,15 @@
+my_list = [20,15,9,10,-1,0]
+names = ["hesam","mahdi","kazem",22,True]
+
+my_new_list=my_list.pop()
+
+my_list.reverse()
+
+my_list.append(33)
+
+my_list.sort()
+
+my_list[1] = 32
+
+print(my_list)
+
